632b0b4aa0db4445a58a252746e3d02130c5d3b2
Simon Y <therealkjai139@gmail.com>
2022-10-03 21:32:23 -0400

    update

--- javascript.js
+++ javascript.js
@@ -4,7 +4,17 @@ class Knight {
         this.moves = null
     }
 
+    
+}
+
+class Chessboard {
+    constructor(){
+        this.root = null
+        
+    }
+
     calculateMoves = ([x, y]) => {
+        let knight = new Knight([x,y])
         let moves = []
         let newX = x - 1
         let newY = y + 2
@@ -63,26 +73,77 @@ class Knight {
         if (checkBoardRange(newX, newY) == true){
             moves.push([newX, newY])
         }
-
-        return moves
+        knight.moves = moves
+        return knight
     }
+
+    containsObj = (obj, arr) => {
+        for (let i = arr.length; i> 0; i--){
+            if (arr[i] == obj){
+                return true
+            }
+            
+        }
+        return false
+    }
+   
+
+    knightMoves = ([startX, startY], [endX, endY], level=0, visited =[], queue=[], found = false) => {
+        
+        
+        if (found == true) {
+            // console.log('moves taken:', level)
+            return
+        }
+        queue.shift()
+        level += 1
+        
+        if (this.containsObj(([startX, startY]), visited) == true) {
+
+            return
+        } else {
+            visited.push([startX, startY])
+        }
+        this.root = this.calculateMoves([startX, startY])
+        console.log(this.root.moves[0])
+        this.root.moves.forEach(element => {
+            if (element[0] == endX && element[1] == endY){
+                
+                found = true
+                return
+            }
+            if (this.containsObj([element[0], element[1]], visited) == false && this.containsObj([element[0], element[1]], queue) == false ){
+                queue.push([element[0], element[1]])
+                
+            }
+        });
+        
+        console.log('q',queue)
+        console.log('v', visited)
+
+        console.log(queue[0][0], queue[0][1])
+        while (queue.length > 1) {
+            this.knightMoves([queue[0][0], queue[0][1]], [endX, endY], level, visited, queue, found)
+        }
+    
+        
+
+        
+
+
+
+    
+        
+
+        
+        
+        
+    
+    }
+    
+
 }
 
-class Chessboard {
-    constructor(){
-        this.root = null
-        this.x = 7
-        this.y = 7
-    }
+let newBoard = new Chessboard
 
-    constructBoard = ([x, y]) => {
-
-    }
-}
-
-let newK = new Knight([4,3])
-console.log(newK.moves)
-
-
-let secondK = new Knight([0,0])
-console.log(secondK.moves)
+newBoard.knightMoves([3,3], [4,3])
